114bba8b0aaaff31896eb0047747b78bd38b1776
nanduf <122034648+nanduf@users.noreply.github.com>
2024-11-05 13:56:29 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace App.Clinic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PatientsClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//Patients");
+        }
+
+        private async void PhysiciansClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//Physicians");
+        }
+
+        private async void AppointmentsClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//Appointments");
+        }
+    }
+}
